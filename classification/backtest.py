026cb8271dffb742fbb7e1d71373ab9d2e6078f0
Sarit Maitra <sarit.maitra@gmail.com>
2021-03-07 21:30:00 +0800

    Create backtest.py

---
+++ classification/backtest.py
@@ -0,0 +1,20 @@
+!pip install pyforest
+from pyforest import *
+import datetime, pickle, copy, warnings
+!pip install cryptocompare
+import cryptocompare
+from time import time
+from datetime import datetime
+from pandas import DataFrame, concat
+from sklearn import metrics, preprocessing
+from math import sqrt
+!pip install pyfolio
+import pyfolio as pf
+!pip install backtrader
+import backtrader as bt
+from backtrader.feeds import PandasData
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import sys, logging, json, pprint, requests
+from google.colab import files
+
+print('Complete....')
